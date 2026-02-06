17f19c1080ffcaf7978ec44d780c17be35138092
rakib <rakibco66@gmail.com>
2026-02-06 12:03:37 +0600

    lern string is imutable

---
+++ string.js
@@ -0,0 +1,22 @@
+// const country = 'bangladesh';
+
+// const division = 'newkhale';
+
+// const district = `B-Baria`;
+
+// const thana = new String('Demra');
+
+// console.log(typeof thana)
+
+// console.log(thana)
+
+// const number = [34, 55, 23, 65];
+
+// console.log(number.length)
+
+// console.log(number[1])
+
+const captial = 'Dh aka';
+
+console.log(captial.length)
+console.log(captial[3])
