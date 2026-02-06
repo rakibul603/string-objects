1f2ddf825b0f9184cf23572939b486004c901f47
rakib <rakibco66@gmail.com>
2026-02-06 12:32:57 +0600

    learn string case sencitive

---
+++ case.js
@@ -0,0 +1,29 @@
+// const school = 'RAJ UK Uttara Model School';
+
+// console.log(school.toLocaleLowerCase())
+
+// const subject = 'Chemistry';
+
+// const book = "chemistry";
+
+// if (subject.toLocaleLowerCase() === book.toLocaleLowerCase() ) {
+//     console.log('I am reading book')
+// }
+// else{ 
+//     console.log('hudai hudai pistra ultai')
+// }
+
+
+const drink = ' water';
+
+const liquid = '  water  ';
+
+
+if (drink.trim() === liquid.trim()){
+    console.log('pani ')
+}
+else {
+    console.log('parin nai ')
+}
+
+
