ef675d702eab0d29290f724e325485c66e2c5793
rakib <rakibco66@gmail.com>
2026-02-06 12:50:26 +0600

    learn split and join

---
+++ slice.js
@@ -0,0 +1,24 @@
+// const addres = 'andorkilla';
+
+// const part = addres.slice(1, 4)
+
+// console.log(addres)
+
+// console.log(part)
+
+
+// const sentence = 'I am a good and hardworking person.';
+
+// console.log(sentence.split(" "))
+
+const friendsStr = 'rahim,kahim,dahim,lahim,fahim';
+
+const firend = friendsStr.split(',');
+// console.log(firend)
+
+const reaFrined =  [ 'rahim', 'kahim', 'dahim', 'lahim', 'fahim' ];
+
+console.log(reaFrined.join())
+
+console.log(reaFrined.join(' |'))
+
