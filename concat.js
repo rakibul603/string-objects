2903b521ffeecc6ed2b148b97b538428f6c3adc0
rakib <rakibco66@gmail.com>
2026-02-13 09:54:08 +0600

    learn str concat slice and join

---
+++ concat.js
@@ -0,0 +1,11 @@
+const first = "Rakib";
+const last = "hasan";
+
+const fullName =  first + " " + last;
+
+console.log(fullName)
+
+const fullName2 = first.concat(" ",last)
+console.log(fullName2)
+
+console.log(last.includes('n'))
---
+++ slice.js
@@ -0,0 +1,20 @@
+const address = 'Barisal';
+
+const part = address.slice(2, 6)
+
+console.log(part)
+
+const sentence =  "I am a good and hardwoking persong";
+
+console.log(sentence.split(" "))
+
+
+const friendStr = "Rahim, Kahim, Dahim, Lahim, Fahim";
+
+const friends = friendStr.split(",");
+
+console.log(friends)
+
+const realFriend = [ 'Rahim', ' Kahim', ' Dahim', ' Lahim', ' Fahim' ];
+
+console.log(realFriend.join(""))
